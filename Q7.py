695e3d5f29dc0954cd0cad21ff1e04db1574a600
freshprinceofcoders <milanosolanki27@gmail.com>
2020-12-24 14:51:53 +0000

    done

--- Q7.py
+++ Q7.py
@@ -1,12 +1,17 @@
-def is_prime(previous_primes, primte_candidate):
-    for previous_prime in previous_primes:
-        if primte_candidate < previous_prime:
-            return False
-        if primte_candidate %  previous_prime == 0:
-            return False
+from math import sqrt
+from itertools import count, islice
 
-    return True
+# def is_prime(previous_primes, primte_candidate):
+#     for previous_prime in previous_primes:
+#         if primte_candidate < previous_prime:
+#             return False
+#         if primte_candidate %  previous_prime == 0:
+#             return False
 
+#     return True
+
+def is_prime_fast(n):
+    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n)-1)))
 
 
 primes = []
@@ -15,7 +20,7 @@ counter = 0
 primes.append(2)
 while len(primes) < 10001:
     primte_candidate += 1
-    if is_prime(primes, primte_candidate):
+    if is_prime_fast(primte_candidate):
         primes.append(primte_candidate)
 
 print(primes[len(primes) -1])
